25a5e7288cdd2b44f0dea9769b04dec4d2f80608
Litao Chen <litao.chen@speer.io>
2024-03-18 16:19:33 -0400

    feat: nav bar styles (#1)

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,15 +1,24 @@
 import React from "react";
+import {LitUpBorderButton} from "@/components/ui/lit-up-border-button";
 
 export default function NavBar() {
     return (
-        <nav className="flex w-screen p-10 justify-between absolute top-0 left-0 z-[100]">
-            <h1>
+        <nav className="flex w-screen p-6 justify-between items-center absolute top-0 left-0 z-[100]">
+            <h1 className="font-sans font-bold text-white text-2xl">
                 Elaborate
             </h1>
-            <ul className="flex">
-                <li className="pr-5">Sign In</li>
-                <li>Request beta access</li>
-            </ul>
+          <div className="md:flex pb-2 items-center hidden">
+            <button className="p-[3px] md:pr-5 pr-2" >
+              <div
+                className="px-4 py-2  bg-transparent rounded-full  relative group transition duration-200 text-white hover:opacity-30 font-sans lg:text-base md:text-sm text-xs"
+              >
+                Sign In
+              </div>
+            </button>
+            <LitUpBorderButton>
+              Request beta access
+            </LitUpBorderButton>
+          </div>
         </nav>
     );
 }
