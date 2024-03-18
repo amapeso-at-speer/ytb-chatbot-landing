985ddf897a8bf9ff59674e24b59f91826f7f6825
amapeso-at-speer <angelica.mapeso@speer.io>
2024-03-18 16:29:41 -0400

    continue on main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,13 +36,25 @@ export default function Home() {
         ]}/>
       </div> */}
       <BackgroundGradientAnimation>
-        <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-          {/* <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-            Gradients X Animations
-          </p> */}
-          <h1>Lorem ipsum dolar delta</h1>
-          {/* <p>Text sample stuff here</p>
-          <button>Some random button</button> */}
+        <div className="absolute z-50 inset-0 flex items-center justify-center">
+          <div className="flex flex-col md:flex-row text-white px-4 max-w-screen-xl">
+            {/* <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+              Gradients X Animations
+            </p> */}
+            <div className="mr-10 flex-1">
+              <h1 className="text-3xl md:text-4xl lg:text-7xl font-bold">Lorem ipsum dolar delta</h1>
+              <p className="text-xl md:text-3xl lg:text-5xl">Text sample stuff here</p>
+              <button className="bg-white/10 py-3 px-6 mt-10 rounded-full font-bold outline outline-1">Request beta access</button>
+            </div>
+            <div className="flex-1 pointer-events-none">
+              {/* <div >
+                <iframe src="https://giphy.com/embed/7OXm7rFOJ0ARoeXOCV" width="100%" height="100%" className="absolute giphy-embed" frameBorder="0" allowFullScreen></iframe>
+              </div> */}
+              {/* <p><a href="https://giphy.com/gifs/warcraft-video-games-world-of-warcraft-7OXm7rFOJ0ARoeXOCV">via GIPHY</a></p> */}
+              <div>Speech bubble 1</div>
+              <div>Speech bubbble 2 - Lorem ipsum dolar delta</div>
+            </div>
+          </div>
         </div>
       </BackgroundGradientAnimation>
     </main>
