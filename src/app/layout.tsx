66eccc002c7cb40aea6d27c3ed93d7f666126641
amapeso-at-speer <angelica.mapeso@speer.io>
2024-04-05 11:49:18 -0400

    fix footer styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,22 +20,25 @@ export default function RootLayout({
         <main className="w-full flex-1 flex flex-col items-center">
           {children}
         </main>
-        <footer className="flex justify-between items-end lg:items-center mt-10 px-5 pb-10 w-full max-w-screen-xl">
-          <div className="flex flex-col lg:flex-row text-white">
-            <a href="tel:+1 416 453 3812">+1 416 453 3812</a>
-            <span className="hidden lg:block mx-3">|</span>
-            <a href="mailto:create@speer.io">create@speer.io</a>
-          </div>
-          <div className="mx-5 text-light-blue">Copyright © 2024 Speer Technologies Inc. All Rights Reserved.</div>
-          <div className="flex flex-col lg:flex-row text-right">
-            <a href="https://www.linkedin.com/company/reframethefuture" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <span className="hidden lg:block mx-3">|</span>
-            <a href="https://www.instagram.com/speertech/" target="_blank" rel="noopener noreferrer">Instagram</a>
-            <span className="hidden lg:block mx-3">|</span>
-            <a href="https://facebook.com/speertech" target="_blank" rel="noopener noreferrer">Facebook</a>
-            <span className="hidden lg:block mx-3">|</span>
-            <a href="https://www.tiktok.com/@speertech?lang=en" target="_blank" rel="noopener noreferrer">Tiktok</a>
-          </div>
+        <footer className="flex flex-col mt-10 px-5 pb-10 w-full max-w-screen-xl">
+          <div className="flex flex-col md:flex-row flex-1 justify-between items-center lg:items-start xl:items-center">
+            <div className="flex flex-row text-white mb-4 md:mb-0">
+              <a href="mailto:create@speer.io">create@speer.io</a>
+              <span className="mx-3">|</span>
+              <a href="tel:+1 416 453 3812">+1 &#40;416&#41; 453&#45;3812</a>
+            </div>
+            <div className="hidden lg:block lg:w-1/3 xl:w-auto mx-5 text-light-blue text-center">Copyright © 2024 Speer Technologies Inc. All Rights Reserved.</div>
+            <div className="flex text-center mb-2  lg:mb-0">
+              <a href="https://www.linkedin.com/company/reframethefuture" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+              <span className="mx-3">|</span>
+              <a href="https://www.instagram.com/speertech/" target="_blank" rel="noopener noreferrer">Instagram</a>
+              <span className="mx-3">|</span>
+              <a href="https://facebook.com/speertech" target="_blank" rel="noopener noreferrer">Facebook</a>
+              <span className="mx-3">|</span>
+              <a href="https://www.tiktok.com/@speertech?lang=en" target="_blank" rel="noopener noreferrer">Tiktok</a>
+            </div>
+          </div> 
+          <div className="block lg:hidden mx-5 text-light-blue text-center">Copyright © 2024 Speer Technologies Inc. All Rights Reserved.</div>
         </footer>
       </body>
     </html>
